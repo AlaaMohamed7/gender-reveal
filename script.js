2314b9e6e74f74d245cc1f3d27b0a1591fcfaf0c
Alaa <128741749+AlaaMohamed7@users.noreply.github.com>
2024-09-22 12:40:35 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ const countdownTimer = setInterval(() => {
         clearInterval(countdownTimer);
         startCelebration(); // Trigger the celebration
     }
-}, 1000000);
+}, 1000);
 
 // Function to start the celebration
 function startCelebration() {
